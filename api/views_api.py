ac63aca160c9b2e8bb40cd564e3d00e8f1aa1f07
YolkinEgor <yolkinegor@gmail.com>
2023-02-05 23:12:09 +0500

    Добавление графика переходов и уникальные переходы в статистику ссылки. Изменения в дизайне, рефакторинг кода.

--- api/views_api.py
+++ api/views_api.py
@@ -1,11 +1,10 @@
-import datetime
-
 from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView
 from api.serializers import CutLinkSerializer
 from api.base.response import CustomResponse
 from django.shortcuts import get_object_or_404
 from api.models import Link, UniquePassed
+import datetime
 
 
 class CustomCreateAPIView(CreateAPIView):
@@ -41,20 +40,25 @@ class GetLinkAPIView(APIView):
     def get(self, request, *args, **kwargs):
         model = get_object_or_404(Link.objects.filter(key=kwargs['key']))
         unique_passed = UniquePassed.objects.filter(link=model).count()
-        response_data = {'passed': model.passed,
-                         'unique_passed': unique_passed}
+        response_data = {
+            'passed': model.passed,
+            'unique_passed': unique_passed
+        }
         return CustomResponse(response_data).make_response()
 
 
 class GetLinkPassedAPIView(APIView):
+    """Get link passed."""
+
     def get(self, request, *args, **kwargs):
         link_model = get_object_or_404(Link.objects.filter(key=kwargs['key']))
 
         now_date = datetime.date.today()
         date_interval = now_date - datetime.timedelta(days=7)
-        unique_passed_models = UniquePassed.objects.filter(created__gte=date_interval,
-                                                           created__lte=now_date,
-                                                           link=link_model)
+        unique_passed_models = UniquePassed.objects.filter(
+            created__gte=date_interval,
+            created__lte=now_date,
+            link=link_model)
         response_data = {}
         for unique_passed in unique_passed_models:
             created = str(unique_passed.created)
